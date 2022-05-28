1004f050a39ca6ca8c96cb5fb0e2db0e3a2c9b49
Tukimiya1012 <eastjapan.1012@gmail.com>
2022-05-28 22:46:00 +0900

    var1.1
    平行移動の追加

--- main.js
+++ main.js
@@ -10,8 +10,10 @@ var V2D = function(x,y){
 };
 
 var center = new V(canvas.width / 2, canvas.height / 2 ,canvas.height / 2);
+var hidariue = new V(canvas.getBoundingClientRect().left, canvas.getBoundingClientRect().top, canvas.getBoundingClientRect().top);
 
 var Stars = [
+  new V(center.x + 0,center.y + 0,center.z + 0),
   new V(center.x + 20,center.y + 30,center.z + 60),
   new V(center.x + 50,center.y + 20,center.z + 70),
   new V(center.x + 40,center.y + 30,center.z + 50),
@@ -28,13 +30,14 @@ var Lines = [
   [Stars[3],Stars[4]],
   [Stars[3],Stars[5]],
   [Stars[2],Stars[6]],
+  [Stars[5],Stars[7]]
 ];
 
 function project(M){
   return new V2D(M.x,M.z);
 }
 
-function render(line_arry, ctx, dx, dy) {
+function render(line_arry, ctx) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   for (var i=0, n = line_arry.length; i<n; ++i){
 
@@ -78,12 +81,21 @@ function render(line_arry, ctx, dx, dy) {
 
   //event
   var mousedown = false;
+  var pan = false;
   var mx = 0;
   var my = 0;
+  var pan_x = 0;
+  var pan_y = 0;
 
   canvas.addEventListener("mousedown", initMove);
   document.addEventListener("mousemove", move);
   document.addEventListener("mouseup", stopMove);
+  document.addEventListener("wheel", zoom, false);
+
+
+  canvas.oncontextmenu = function(evt){
+    return false;
+  }
 
   //rotate
   function rotate(M, center, theta, phi){
@@ -99,13 +111,38 @@ function render(line_arry, ctx, dx, dy) {
     M.x = ct * x - st * cp * y + st * sp * z + center.x;
     M.y = st * x + ct * cp * y - ct * sp * z + center.y;
     M.z = sp * y + cp * z + center.z;
+    console.log(M.x);
   }
 
+  //pan
+  function pan_move(M,mousex,mousey,cen, evt, ctx){
+    var scene = project(cen);
+    var nx = evt.clientX;
+    var ny = evt.clientY;
+    
+    ctx.translate(-(mousex-nx)/8,-(mousey-ny)/8);
+    console.log(nx);
+  }
+
+
   function initMove(evt){
-    clearTimeout(autrotate_timeout);
-    mousedown = true;
-    mx = evt.clientX;
-    my = evt.clientY;
+    if(typeof evt === "object"){
+      switch(evt.button){
+        case 0:
+          clearTimeout(autrotate_timeout);
+          mousedown = true;
+          mx = evt.clientX;
+          my = evt.clientY;
+          break;
+        case 2:
+          pan = true;
+          pan_x = evt.clientX;
+          pan_y = evt.clientY;
+          break;
+      }
+    }else{
+      return;
+    }
   }
 
   function move(evt){
@@ -120,12 +157,46 @@ function render(line_arry, ctx, dx, dy) {
       mx = evt.clientX;
       my = evt.clientY;
 
-      render(Lines, ctx, dx,dy);
+      render(Lines, ctx);
+    }
+
+    if(pan){
+      var mousex
+
+      for(var j = 0; j<Stars.length; ++j){
+        pan_move(Stars[j], pan_x, pan_y, center, evt, ctx);
+      }
+      pan_x = evt.clientX;
+      pan_y = evt.clientY;
+      render(Lines, ctx);
+      
     }
   }
 
   function stopMove(){
     mousedown = false;
+    pan = false;
+  }
+
+  //zoom
+  function zoom(){
+    if(event.wheelDelta > 0){
+      var sca = 1.1
+      zmx = center.x * 1.1;
+      zmy = center.y * 1.1;
+    }else{
+      var sca = 0.9
+      zmx = center.x * 0.9;
+      zmy = center.y * 0.9;
+    }
+    ctx.beginPath();
+    ctx.scale(sca, sca);
+    //ctx.translate(zmx,zmy);
+    
+    render(Lines, ctx, dx,dy);
+    ctx.closePath();
+    ctx.stroke();
+
   }
 
   function autorotate(){
